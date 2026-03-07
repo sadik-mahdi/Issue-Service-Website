a40745bcaf1f21be7d14f947b1a13a97df7f12d5
sadik-mahdi <sadikmahadi06@gmail.com>
2026-03-07 19:51:55 +0600

    created index.html as login page and added functionality for the page.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+document.getElementById("login-btn").addEventListener("click",function(){
+  const nameInput = document.getElementById("userName");
+  const userName = nameInput.value;
+  console.log(userName);
+
+  const passwordInput = document.getElementById("userPassword");
+  const userPassword = passwordInput.value;
+  console.log(userPassword);
+
+  if(userName == "admin" && userPassword == "admin123"){
+    alert("login success");
+    window.location.replace("home.html");
+  }else{
+    alert("login failed <br> <span>Enter correct username and password</span>");
+  }
+
+});
